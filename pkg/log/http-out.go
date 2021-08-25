7a93de4b02906f64f1fa8054d4e16d3aa53cbe90
Claas Störtenbecker <github@clstb.codes>
2021-08-25 17:29:06 +0200

    fix: unbreak consumers (#118)
    * fix: unbreak consumers
    
    * feat: add comments of deprecation

---
+++ pkg/log/http-out.go
@@ -0,0 +1,42 @@
+package log
+
+import (
+	"net/http"
+	"time"
+)
+
+// Transport is deprecated, please use go-svc/pkg/transport
+type Transport struct {
+	next   http.RoundTripper
+	logger *Logger
+}
+
+// RoundTrip is deprecated, please use go-svc/pkg/transport
+func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if req.Context() != nil {
+		traceID, _ := req.Context().Value(TraceIDContextKey).(string)
+		if traceID != "" {
+			req.Header.Set(TraceIDHeaderKey, traceID)
+		}
+	}
+
+	reqTime := time.Now()
+	_ = t.logger.HttpOutReq(req)
+
+	resp, err := t.next.RoundTrip(req)
+
+	_ = t.logger.HttpOutResponse(req, resp, reqTime)
+
+	return resp, err
+}
+
+// LoggedTransport is deprecated, please use go-svc/pkg/transport
+func (l *Logger) LoggedTransport(next http.RoundTripper) *Transport {
+	if next == nil {
+		next = http.DefaultTransport
+	}
+	return &Transport{
+		next:   next,
+		logger: l,
+	}
+}
