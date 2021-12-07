f3ae2ffd4b9f64e8a28621d71d5d557f749eac4f
Philipp <philipp.lange@data4life.care>
2021-12-07 11:22:14 +0100

    Add internal vega v2 client (#155)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- client: vega v2 client
+
 ### Changed
 
 - [JWT pkg]: unknown scopes are ignored by `Scope`'s `Scan` and `UnmarshalJSON` methods (but not by `NewScope` method).
---
+++ pkg/client/vegaInternal.go
@@ -0,0 +1,73 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gesundheitscloud/go-svc/pkg/logging"
+
+	"github.com/gofrs/uuid"
+)
+
+// EmailTenant is a pair of named strings
+type EmailTenant struct {
+	Email    string `json:"email"`
+	TenantID string `json:"tenantID"`
+}
+
+type VegaInternal interface {
+	// ResolveEmailV2 resolves userID into pair: (email address, tenant ID)
+	ResolveEmailV2(ctx context.Context, userIDs []uuid.UUID) (map[uuid.UUID]EmailTenant, int, error)
+}
+
+var _ VegaInternal = (*VegaInternalAPI)(nil)
+var userAgentVega = "cds-notification.client.VegaInternalAPI"
+
+type VegaInternalAPI struct {
+	svcAddr   string
+	svcSecret string
+	caller    string
+}
+
+func NewVegaInternalAPI(svcAddr, svcSecret, caller string) *VegaInternalAPI {
+	if caller == "" {
+		caller = "unknown"
+	}
+	return &VegaInternalAPI{
+		svcAddr:   svcAddr,
+		svcSecret: "Bearer " + svcSecret,
+		caller:    caller,
+	}
+}
+
+// ResolveEmailV2 returns mapping of userID -> (email, tenantID). Uses tenantID returned from vega
+func (c *VegaInternalAPI) ResolveEmailV2(ctx context.Context, userIDs []uuid.UUID) (map[uuid.UUID]EmailTenant, int, error) {
+	contentURL := fmt.Sprintf("%s/internal/users/api/v2/email", c.svcAddr)
+	emailMap := make(map[uuid.UUID]EmailTenant)
+	bodyBytes, err := json.Marshal(userIDs)
+	if err != nil {
+		logging.LogErrorfCtx(ctx, err, "marshalling user IDs failed")
+		return emailMap, 0, err
+	}
+	body := bytes.NewBuffer(bodyBytes)
+	// additionally accept 404s as null-results
+	bReply, statusCode, err := call(ctx, contentURL, "POST", c.svcSecret, userAgentVega, body, http.StatusOK, http.StatusNotFound)
+	if err != nil {
+		logging.LogErrorfCtx(ctx, err, "resolving user email over APIv2 failed")
+		return emailMap, statusCode, err
+	}
+	// no need to unmarshal on 404s
+	if statusCode == 404 {
+		// we expect any error on 404s
+		return emailMap, statusCode, fmt.Errorf("server replied with 404 and body: %s", string(bReply))
+	}
+	if err := json.Unmarshal(bReply, &emailMap); err != nil {
+		logging.LogErrorfCtx(ctx, err, "failed unmarshalling vega reply into EmailTenant. Reply body: %s", string(bReply))
+		return emailMap, statusCode, err
+	}
+	logging.LogDebugfCtx(ctx, "Vega '%s' returned emailMap: %+v", contentURL, emailMap)
+	return emailMap, statusCode, nil
+}
