9e1610d09bb4238e5f27007dd7bf27a4fbe4286b
David Weese <david.weese@data4life.care>
2022-02-28 14:19:41 +0100

    simulate cancelled requests and failing service (#190)

--- pkg/client/featureFlaggingMock.go
+++ pkg/client/featureFlaggingMock.go
@@ -2,20 +2,37 @@ package client
 
 import (
 	"context"
+	"errors"
+	"time"
 )
 
 var _ FeatureFlagging = (*FeatureFlaggingMock)(nil)
 
 type FeatureFlaggingMock struct {
 	EnabledKeys []string
+	Delay       time.Duration
 }
 
 // Get fetches a single setting for a single user
 func (f *FeatureFlaggingMock) Get(ctx context.Context, key string) (bool, error) {
-	for _, enabledKey := range f.EnabledKeys {
-		if key == enabledKey {
-			return true, nil
+	select {
+	case <-time.After(f.Delay):
+		for _, enabledKey := range f.EnabledKeys {
+			if key == enabledKey {
+				return true, nil
+			}
 		}
+		return false, nil
+
+	case <-ctx.Done():
+		return false, ctx.Err()
 	}
-	return false, nil
+
+}
+
+type FeatureFlaggingErrorMock struct{}
+
+// Get fetches a single setting for a single user
+func (f *FeatureFlaggingErrorMock) Get(ctx context.Context, key string) (bool, error) {
+	return false, errors.New("Not reachable")
 }
